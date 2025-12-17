6bb851c3457cee29ea111902bbc32365427f3efb
laurens <26894824+lvandenbrink@users.noreply.github.com>
2025-12-17 16:40:11 +0100

    site improvements

--- site/mqtt.py
+++ site/mqtt.py
@@ -4,8 +4,8 @@ import paho.mqtt.publish as publish
 
 mqtt_broker = os.getenv("MQTT_BROKER", "localhost")
 mqtt_port = int(os.getenv("MQTT_PORT", "1883"))
-mqtt_username = os.getenv("MQTT_USERNAME", "")
-mqtt_password = os.getenv("MQTT_PASSWORD", "")
+mqtt_user = os.getenv("MQTT_USERNAME", "")
+mqtt_pass = os.getenv("MQTT_PASSWORD", "")
 mqtt_topic = os.getenv("TEMPERATURE_TOPIC", "sensor/climate/temperature")
 
 
@@ -13,9 +13,7 @@ class Mqtt:
 
     def execute(self, device, command, data):
         topic = f"device/{device}/{command}"
-        auth = None
-        if mqtt_username:
-            auth = {"username": mqtt_username, "password": mqtt_password}
+        auth = {"username": mqtt_user, "password": mqtt_pass} if mqtt_user else None
         message = json.dumps(data)
         result = publish.single(topic, message, hostname=mqtt_broker, port=mqtt_port, auth=auth)
         success = "successful" if result is None else "unsuccessful"
